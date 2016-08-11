7cc5ff8dd16308921bd4b244e4a3aafe2583c209
Rex Recio <rexrecio@gmail.com>
2016-08-11 23:54:36 +0800

    Add the start shortcuts.

---
+++ eve/starteve.sh
@@ -0,0 +1,1 @@
+docker run -it -p 5000:5000 -v /home/rjr/eve:/source --name eve_instance --network dockernet -d eve:1.0
---
+++ mongodb/startmongodb.sh
@@ -0,0 +1,1 @@
+docker run -p 27017:27017 --name mongodb_instance --network dockernet -d mongodb:1.0
